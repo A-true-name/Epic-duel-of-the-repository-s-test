bd12f8da09f59f3f502897d082883871d9783383
kaufmha <kaufmha@whs.school.nz>
2023-02-14 15:10:34 +1300

    Putting the repository in git Gui

---
+++ IntTest.java
@@ -0,0 +1,58 @@
+
+/**
+ * Write a description of class IntTest here.
+ *
+ * @author (Hamish Kaufman)
+ * @version (Version 1(9/02/2023)
+ */
+public class IntTest
+{
+    // instance variables - replace the example below with your own
+    private int x;
+    public int year=2020;
+    public int y;
+    {
+        x -= 6+3;
+        {int y=24;}
+        y += 5;
+    }
+    public int ham;
+    {
+        x -= 6+3;
+        {int ham=5+132-63;}
+        ham *= 5;
+    }
+        public int mouse;
+    {
+        x -= 6+3;
+        {int mouse=5-12*5/2;}
+        mouse /= 13;
+    }
+    public int splle;
+    {
+        x -= 6+3;
+        {int splle=5+16-42/2;}
+        splle =+ 91;
+    }
+    /**
+     * Constructor for objects of class IntTest
+     */
+    public IntTest()
+    {
+        // initialise instance variables
+        x += x+5-1;
+        System.out.println(x);
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
